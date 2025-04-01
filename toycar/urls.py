dbc93fd04c93f93ab60247a4e9867defba966268
jamesmanglapus-gilberd <jvgilberd@gmail.com>
2025-04-01 20:54:58 +0800

    add new pages, fix images. Final fronted submission. TODO - BACKEND SHOP, LOGIN, CHECKOUT integration.

--- toycar/urls.py
+++ toycar/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('item/<str:item_id>/', views.item, name='item'),
     path('checkout/', views.checkout, name='checkout'),
     path('settings/', views.settings, name='settings'),
+    path('classic/', views.classic, name='classic'),
+    path('speed/', views.speed, name='speed'),
+    path('offroad/', views.offroad, name='offroad'),
+    path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
+    path('care/', views.care, name='care'),
 ]
--- toycar/views.py
+++ toycar/views.py
@@ -22,8 +22,26 @@ def shop(request):
 def settings(request):
     return render(request, "settings.html")
 
+def speed(request):
+    return render(request, "speed.html")
+
+def offroad(request):
+    return render(request, "offroad.html")
+
+def classic(request):
+    return render(request, "classic.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def faq(request):
+    return render(request, "faq.html")
+
+def care(request):
+    return render(request, "care.html")
+
 def item(request, item_id):
-    item_name = "placeholder" # TODO implement, probably with db?
-    item_desc = "foo bar baz or whatever they say"
+    item_name = "Toy Car" # TODO implement, probably with db?
+    item_desc = "This will be fixed in the back end"
     return render(request, "shoptemplate.html",
                   {"item_id": item_id, "item_name": item_name, "item_desc": item_desc})
