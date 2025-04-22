4ba12101ddfbf3e64500f97deb93e8f68ea00e46
Hawthorn Emerald <hawthornemerald100@gmail.com>
2025-04-22 13:39:41 +0800

    vercel

--- toycar/firebase.py
+++ toycar/firebase.py
@@ -1,24 +1,42 @@
 from pathlib import Path
 
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, db
 from dotenv import load_dotenv
-import os
+def get_firebase_credentials():
+    # Try loading from file path (local dev)
+    cred_path = os.environ.get("FIREBASE_CREDENTIALS_PATH")
+    if cred_path and os.path.exists(cred_path):
+        return credentials.Certificate(cred_path)
+    # Else, try from string (Vercel env var)
+    firebase_creds_str = os.environ.get("FIREBASE_CREDENTIALS_JSON")
+    if firebase_creds_str:
+        firebase_creds_dict = json.loads(firebase_creds_str)
+        return credentials.Certificate(firebase_creds_dict)
+    raise ValueError("Firebase credentials not found.")
+    # Initialize only if not already initialized
+if not firebase_admin._apps:
+    cred = get_firebase_credentials()
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://toycar-c1ce2-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    })
+##
 
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# load_dotenv(dotenv_path=BASE_DIR / '.env')
+#
+# FIREBASE_CRED_PATH = os.environ.get('FIREBASE_CRED_PATH')
+#
+# current_directory = os.path.dirname(os.path.abspath(__file__))
+# json_path = os.path.join(current_directory,
+# FIREBASE_CRED_PATH)
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(dotenv_path=BASE_DIR / '.env')
-
-FIREBASE_CRED_PATH = os.environ.get('FIREBASE_CRED_PATH')
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
-json_path = os.path.join(current_directory,
-FIREBASE_CRED_PATH)
-
-cred = credentials.Certificate(json_path)
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://toycar-c1ce2-default-rtdb.asia-southeast1.firebasedatabase.app/'
-})
+# cred = credentials.Certificate(json_path)
+# firebase_admin.initialize_app(cred, {
+#     'databaseURL': 'https://toycar-c1ce2-default-rtdb.asia-southeast1.firebasedatabase.app/'
+# })
 
 # Reference to the Realtime Database
 database_ref = db.reference()
--- toycar_django/wsgi.py
+++ toycar_django/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toycar_django.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+'EdTechSubscription.settings')
 application = get_wsgi_application()
